964b4a4acb882343ebc860196a713075c1d1333d
vominhtrieu <minhtrieuvo600@gmail.com>
2021-01-31 20:26:53 +0700

    Add procedure and text blocks, fixed trashcan not display

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,6 @@ function App() {
     <Layout style={{ height: "100vh" }}>
       <BlocklyComponent
         readOnly={false}
-        media="media/"
         move={{
           scrollbars: true,
           drag: true,
@@ -16,19 +15,36 @@ function App() {
         }}
         initialXml={`<xml xmlns="http://www.w3.org/1999/xhtml"></xml>`}
       >
+        <Category name="Variables" categorystyle="variable_category">
+          <Block type="variables_get"></Block>
+          <Block type="variables_set"></Block>
+        </Category>
+        <Category name="Procedures" categorystyle="procedure_category">
+          <Block type="procedures_defnoreturn"></Block>
+          <Block type="procedures_defreturn"></Block>
+          <Block type="procedures_mutatorcontainer"></Block>
+          <Block type="procedures_mutatorarg"></Block>
+          <Block type="procedures_callnoreturn"></Block>
+          <Block type="procedures_callreturn"></Block>
+        </Category>
+        <Category name="Text" categorystyle="text_category">
+          <Block type="text_print"></Block>
+          <Block type="text"></Block>
+          <Block type="text_length"></Block>
+          <Block type="text_multiline"></Block>
+          <Block type="text_join"></Block>
+          <Block type="text_append"></Block>
+        </Category>
         <Category name="Loop" categorystyle="loop_category">
           <Block type="controls_whileUntil" blockstyle="list_blocks"></Block>
           <Block type="controls_repeat"></Block>
+          <Block type="controls_for"></Block>
         </Category>
         <Category name="Logic" categorystyle="logic_category">
           <Block type="controls_if"></Block>
           <Block type="logic_compare"></Block>
           <Block type="logic_operation"></Block>
           <Block type="logic_boolean"></Block>
-        </Category>
-        <Category name="Variables" categorystyle="variable_category">
-          <Block type="variables_get"></Block>
-          <Block type="variables_set"></Block>
         </Category>
         <Category name="Math" categorystyle="math_category">
           <Block type="math_number"></Block>
