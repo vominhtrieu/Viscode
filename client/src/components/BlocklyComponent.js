af1f02b318612a2ff752775b119a0fd0b3fc9a05
vominhtrieu <minhtrieuvo600@gmail.com>
2021-02-01 20:41:53 +0700

    Add procedure and text blocks, fixed trashcan not display

---
+++ client/src/components/BlocklyComponent.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./blocklyComponent.css";
+import Blockly from "blockly/core";
+import "blockly/blocks";
+import BlocklyJavaScript from "blockly/javascript";
+import locale from "blockly/msg/en";
+Blockly.setLocale(locale);
+
+function BlocklyComponent(props) {
+  const toolbox = React.useRef(null);
+  const editor = React.useRef(null);
+  const { initialXml, children, onWorkSpaceChange, ...rest } = props;
+
+  React.useEffect(() => {
+    const workSpace = Blockly.inject(editor.current, {
+      toolbox: toolbox.current,
+      ...rest,
+    });
+    workSpace.addChangeListener(() => {
+      const code = BlocklyJavaScript.workspaceToCode(workSpace);
+      onWorkSpaceChange(code);
+    });
+    if (initialXml) {
+      Blockly.Xml.domToWorkspace(Blockly.Xml.textToDom(initialXml), workSpace);
+    }
+
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <>
+      <xml id="toolbox" is="blockly" style={{ display: "none" }} ref={toolbox}>
+        {children}
+      </xml>
+      <div ref={editor} id="editor"></div>
+    </>
+  );
+}
+
+const Block = ({ children, ...props }) => React.createElement("block", { ...props, is: "blockly" }, children);
+const Category = ({ children, ...props }) => React.createElement("category", { ...props, is: "blockly" }, children);
+
+export { Block, Category };
+export default BlocklyComponent;
---
+++ client/src/components/ExportArea.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Space, Select, Button, Layout } from "antd";
+const { Option } = Select;
+
+function ExportArea({ code, runCode }) {
+  return (
+    <Layout style={{ backgroundColor: "white", width: "100%", height: "100%", display: "flex" }}>
+      <Space style={{ marginLeft: "auto", padding: 5 }}>
+        <Select defaultValue="js" style={{ width: 120 }}>
+          <Option value="js">Javascript</Option>
+          <Option value="cpp">C++</Option>
+          <Option value="py">Python</Option>
+        </Select>
+        <Button type="ghost">Download</Button>
+        <Button type="primary" onClick={runCode}>
+          Run
+        </Button>
+      </Space>
+
+      <p id="codeArea" style={{ overflowY: "auto", borderTop: "1px solid #d9d9d9", whiteSpace: "pre", padding: 5 }}>
+        {code}
+      </p>
+    </Layout>
+  );
+}
+
+export default ExportArea;
